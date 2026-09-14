01a95c7990f427f9f63f59ee002090890e20bb48
karoooo1112 <karoline.fischer@stud.leuphana.de>
2026-09-15 00:32:09 +0200

    Refactor Character class for movement and collision
    Updated character movement and collision detection logic, resized character image, and added hitbox functionality.

--- Final Project/Silent_Walls/code/silent_walls.py
+++ Final Project/Silent_Walls/code/silent_walls.py
@@ -40,34 +40,78 @@ base.fill(base_color)
 #image = "pixil-frame-0(5).png"
 
 # your character
-class Character:
+# collision by Karo Fischer 
+class Character():
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.image = pygame.image.load("pixil-frame-0(5).png").convert_alpha()
-        self.transform_image = pygame.transform.scale(self.image, (50, 70))
-        self.rect = self.transform_image.get_rect(topleft= (x, y))
+        self.transformed_image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.transformed_image.get_rect(topleft=(x, y))
+        self.hitbox = pygame.Rect(x, y + 50, 80, 20) 
 
     def draw(self, screen):
-        screen.blit(self.transform_image, self.rect)
+        screen.blit(self.transformed_image, self.rect)
+        pygame.draw.rect(screen, "red", self.hitbox, 2)
 
-    def move(self):
+    def update_hitbox(self):
+        self.hitbox.x = self.rect.x + 25
+        self.hitbox.y = self.rect.y + 80
+
+    def move(self, objects):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.rect.y -= 3
         if keys[pygame.K_a]:
-            self.rect.x -= 3
-        if keys[pygame.K_s]:
-            self.rect.y += 3
+            self.rect.x -= 5
+            self.update_hitbox()
+            if self.check_collision(objects):
+                self.rect.x +=5
+                self.update_hitbox()
+
         if keys[pygame.K_d]:
-            self.rect.x += 3
+            self.rect.x += 5
+            self.update_hitbox()
+            if self.check_collision(objects):
+                self.rect.x -=5
+                self.update_hitbox()
 
-    def update(self):
-        pass
+        if keys[pygame.K_w]:
+            self.rect.y -= 5
+            self.update_hitbox()
+            if self.check_collision(objects):
+                self.rect.y +=5
+                self.update_hitbox()
 
-    def set_pos(self, x, y):
-        self.rect.topleft = (x, y)
+        if keys[pygame.K_s]:
+            self.rect.y += 5
+            self.update_hitbox()
+            if self.check_collision(objects):
+                self.rect.y -=5
+                self.update_hitbox()
 
+    def check_collision(self, objects):
+        for obj in objects:
+            if self.hitbox.colliderect(obj.rect):
+                return True
+        return False
+
+    def check_walls(self):
+        if self.rect.left < 0:
+            self.hitbox.left = 0
+            self.rect.left = 0
+        if self.rect.right > 980:
+            self.hitbox.left = 980
+            self.rect.right = 980
+        if self.rect.top < 200:
+            self.hitbox.left = 200
+            self.rect.top = 200
+        if self.rect.bottom > 480:
+            self.hitbox.left = 480
+            self.rect.bottom = 480
+
+    def update(self, objects):
+        self.move(objects)
+        #self.update_hitbox()
+        self.check_walls()
 
 
 # I got help from AI for the main Game Loop here
