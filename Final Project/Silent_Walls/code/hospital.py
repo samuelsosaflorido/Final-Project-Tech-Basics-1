d373f9e5ac140454f676930a62b247256e8732e2
samuelsosaflorido <samuelsosaflorido@gmail.com>
2026-09-15 23:38:29 +0200

    Improve Hospital class with interactions and intro text
    Enhanced the Hospital class with intro text and item interactions. Added code selection mechanism for unlocking doors based on character's inventory.

--- Final Project/Silent_Walls/code/hospital.py
+++ Final Project/Silent_Walls/code/hospital.py
@@ -1,10 +1,16 @@
 # the basic room code is by Kathi
+# The rest of the code has been completed by Samuel trying to not to modify anything that has been established before
+# Plus adding making that the items can be used to unlock the areas after the player has completed the game 
 
+import sys
+import random
+import inventory
 import pygame
 from silent_walls_character import *
+from silent_walls_intro_text import *
+from silent_walls_endings import *
 
 pygame.init()
-
 
 
 class Hospital():
@@ -15,24 +21,112 @@ class Hospital():
         self.door2 = DoorC(500, 70)
         self.skeleton = Skeleton(300, 70)
 
-
-
         self.background = pygame.image.load("cell_wall.png").convert_alpha()
         self.background = pygame.transform.scale(self.background, (980, 480))
 
         self.objects = [self.bed, self.table, self.door1, self.door2, self.skeleton]
-
-        self.background = pygame.image.load("cell_wall.png").convert_alpha()
-        self.background = pygame.transform.scale(self.background, (980, 480))
+        self.intro_shown = False
 
     def draw(self, screen):
         screen.blit(self.background, (0, 0))
         for obj in self.objects:
             obj.draw(screen)
 
-    def update(self, character, events):
-        pass
+    def update(self, character, events, screen, clock):
+        if not self.intro_shown:
+            show_intro_text(screen, clock)
+            self.intro_shown = True
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
 
+                if character.rect.colliderect(self.door2.rect):
+                    if character.letters >= 3:
+                        btn_width, btn_height = 160, 50
+                        spacing = 40
+                        start_x = (980 - (btn_width * 3 + spacing * 2)) // 2
+                        btn_y = 260
+
+                        codes = ["734", "379", "715"]
+                        random.shuffle(codes)
+
+                        options = [
+                            {"rect": pygame.Rect(start_x, btn_y, btn_width, btn_height), "code": codes[0]},
+                            {"rect": pygame.Rect(start_x + btn_width + spacing, btn_y, btn_width, btn_height),
+                             "code": codes[1]},
+                            {"rect": pygame.Rect(start_x + (btn_width + spacing) * 2, btn_y, btn_width, btn_height),
+                             "code": codes[2]},
+                        ]
+
+                        overlay = pygame.Surface((980, 480))
+                        overlay.set_alpha(180)
+                        overlay.fill((0, 0, 0))
+
+                        font = pygame.font.Font(None, 36)
+                        selected_code = None
+
+                        while selected_code is None:
+                            mouse_pos = pygame.mouse.get_pos()
+
+                            for opt_event in pygame.event.get():
+                                if opt_event.type == pygame.QUIT:
+                                    pygame.quit()
+                                    sys.exit()
+                                if opt_event.type == pygame.MOUSEBUTTONDOWN and opt_event.button == 1:
+                                    for opt in options:
+                                        if opt["rect"].collidepoint(opt_event.pos):
+                                            selected_code = opt["code"]
+                                            break
+
+                            screen.blit(overlay, (0, 0))
+                            prompt_surf = font.render("Select the correct code:", True, (255, 255, 255))
+                            prompt_rect = prompt_surf.get_rect(center=(490, 180))
+                            screen.blit(prompt_surf, prompt_rect)
+
+                            for opt in options:
+                                is_hovered = opt["rect"].collidepoint(mouse_pos)
+                                bg_color = (90, 90, 90) if is_hovered else (45, 45, 45)
+                                border_color = (255, 255, 255) if is_hovered else (150, 150, 150)
+
+                                pygame.draw.rect(screen, bg_color, opt["rect"])
+                                pygame.draw.rect(screen, border_color, opt["rect"], 2)
+
+                                text_surf = font.render(opt["code"], True, (255, 255, 255))
+                                text_rect = text_surf.get_rect(center=opt["rect"].center)
+                                screen.blit(text_surf, text_rect)
+
+                            pygame.display.flip()
+                            clock.tick(60)
+
+                        if selected_code == "734":
+                            show_ending(screen, clock, "letters_734")
+                        elif selected_code == "379":
+                            show_ending(screen, clock, "letters_379")
+                        elif selected_code == "715":
+                            show_ending(screen, clock, "letters_715")
+
+                        pygame.quit()
+                        sys.exit()
+                    else:
+                        print("For this door it is required a code. Maybe I can find this somewhere")
+
+                elif character.rect.colliderect(self.door1.rect):
+                    if character.keys >= 3:
+                        show_ending(screen, clock, "keys")
+                        pygame.quit()
+                        sys.exit()
+                    else:
+                        print("This door requires three keys to be unlocked. Maybe I need to look somewhere")
+
+                elif character.rect.colliderect(self.bed.rect):
+                    print("This bed gives a strange feeling...")
+
+                elif character.rect.colliderect(self.table.rect):
+                    if character.medication >= 3:
+                        show_ending(screen, clock, "medication")
+                        pygame.quit()
+                        sys.exit()
+                    else:
+                        print("It a weird looking table but seems something is needed here.")
 
 
 class Bed():
@@ -83,6 +177,3 @@ class Skeleton():
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-
-
-
